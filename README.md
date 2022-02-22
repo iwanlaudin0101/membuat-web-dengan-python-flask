edb13951a34492af5652a5716957fe8350a18daa
Iwan La Udin <iwanlaudin0101@gmail.com>
2022-02-22 13:17:36 +0900

    mengubah isi folder app

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # membuat-web-dengan-python-flask
+# web-dengan-python-flask
---
+++ app/__init__.py
@@ -0,0 +1,40 @@
+from flask import Flask
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+from flask_mail import Mail
+
+db = SQLAlchemy()
+migrate = Migrate()
+bcrypt = Bcrypt()
+login_manager = LoginManager()
+login_manager.login_view = 'auth.login'
+login_manager.login_message_category = 'info'
+mail = Mail()
+
+
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(Config)
+    bcrypt.init_app(app)
+    db.init_app(app)
+    migrate.init_app(app, db)
+    mail.init_app(app)
+    login_manager.init_app(app)
+
+    # Model
+    from app.model import user, blog
+
+    # Controller
+    from .controller.auth.routes import auth
+    from .controller.site.routes import site
+    from .controller.errors.handlers import errors
+
+    # Blueprint
+    app.register_blueprint(auth)
+    app.register_blueprint(site)
+    app.register_blueprint(errors)
+
+    return app
